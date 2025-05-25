86043f9d6160054186f412fda43fc85060b1a3f3
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-05-24 20:27:29 -0500

    crear la vista para el formulario de los alumnos

--- proyecto/escuela/views.py
+++ proyecto/escuela/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AlumnoForm, CursoForm, NotaForm
 
-# Create your views here.
+def agregar_alumno(request):
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('agregar_alumno')
+    else:
+        form = AlumnoForm()
+    return render(request, 'agregar_alumno.html', {'form': form})
+
+
