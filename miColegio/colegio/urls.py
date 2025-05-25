e524ad332cf570d067ceea7033eef5212a1a9174
Wilber Jesus Guerra Pilco <wguerra@unsa.edu.pe>
2025-05-24 21:18:07 -0500

    creando urls en el proyecto y en la app

---
+++ miColegio/colegio/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('nuevo_alumno/', views.nuevo_alumno, name='nuevo_alumno'),
+    path('nuevo_curso/', views.nuevo_curso, name='nuevo_curso'),
+    path('nueva_nota/', views.nueva_nota, name='nueva_nota'),
+]
