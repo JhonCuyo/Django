d13bef8ca1d74a190d48b22ab27719cc6fa6e63f
Wilber Jesus Guerra Pilco <wguerra@unsa.edu.pe>
2025-05-24 21:14:23 -0500

    creando vistas en views.py

--- miColegio/colegio/views.py
+++ miColegio/colegio/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AlumnoForm, CursoForm, NotaAlumnoCursoForm
 
-# Create your views here.
+def nuevo_alumno(request):
+    form = AlumnoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('nuevo_alumno')
+    return render(request, 'colegio/formulario.html', {'form': form, 'titulo': 'Nuevo Alumno'})
+
+def nuevo_curso(request):
+    form = CursoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('nuevo_curso')
+    return render(request, 'colegio/formulario.html', {'form': form, 'titulo': 'Nuevo Curso'})
+
+def nueva_nota(request):
+    form = NotaAlumnoCursoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('nueva_nota')
+    return render(request, 'colegio/formulario.html', {'form': form, 'titulo': 'Nueva Nota'})
